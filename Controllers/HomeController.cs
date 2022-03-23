6d79fa633210f04b11b9d8ae9b00a2cad245cf3a
laxedo17 <saracyan78@hotmail.com>
2022-03-23 23:56:32 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiroProxecto.Controllers;
+
+public class HomeController : Controller
+{
+    //accion simple definida por HomeController, chamada Index
+    public ViewResult Index()
+    {
+        int hora = DateTime.Now.Hour;
+        string viewModel = hora < 12 ? "Bos dias" : "Boas noites";
+        return View("MinhaView", viewModel);
+    }
+
+}
